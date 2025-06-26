ede921825f823706bc34f052be3cbd1e5100557a
Simon Casadiego <simoncasadiegochacon@gmail.com>
2025-06-26 01:13:05 -0500

    funcion asignar tutor y mostrarProyectos, revisar comentarios al inicio

--- src/main/UI.java
+++ src/main/UI.java
@@ -3,6 +3,7 @@ package main;
 import app.Estudiante;
 import app.Plataforma;
 import app.Profesor;
+import app.Proyecto;
 import java.util.Scanner;
 
 public class UI {
@@ -75,10 +76,55 @@ public class UI {
     }
     
     public void asignarTutor(){
-        
+        scan.nextLine();
+        System.out.println("Menú de asignación de tutor: ");
+        System.out.println("Desea ver los proyectos activos?(0:Si/1:No): ");
+        byte opcion= this.scan.nextByte();
+        if(opcion==0){
+            this.plataforma.mostrarProyectos();
+        }
+        scan.nextLine();
+        System.out.println("Por favor ingrese el nombre del proyecto: ");
+        String nombreP= this.scan.nextLine();
+        Proyecto proyecto=plataforma.compararNombreP(nombreP);
+        if(proyecto!=null){
+            System.out.println("Desea ver los profesores activos?(0:Si/1:No)");
+            opcion=this.scan.nextByte();
+            if(opcion==0){
+                plataforma.mostrarProfesores();
+            }
+            scan.nextLine();
+            System.out.println("Ingrese el nombre del profesor que desea asignar: ");
+            String nombre= this.scan.nextLine();
+            Profesor profesor=plataforma.compararNombre(nombre);
+            if(profesor!=null){
+                proyecto.agregarProfesor(profesor);
+                System.out.println("Se agregó exitosamente al profesor: "+nombre+" al proyecto "+nombreP);
+            }
+            else{
+                System.out.println("Nombre de profesor inválido");
+            }
+        }
+        else{
+            System.out.println("Nombre de proyecto inválido");
+        }
     }
     
     public void mostrarProyectosEstudiante(){
-
+        scan.nextLine();
+        System.out.println("Menú de proyectos por estudiante: ");
+        System.out.println("Desea ver los estudiantes registrados?(0:Si/1:No) ");
+        byte opcion=this.scan.nextByte();
+        if(opcion==0){
+            plataforma.mostrarEstudiantes();
+        }
+        scan.nextLine();
+        System.out.println("Ingrese el nombre del estudiante: ");
+        String nombreE=this.scan.nextLine();
+        Estudiante estudiante= plataforma.compararNombreE(nombreE);
+        if(estudiante!=null){
+            plataforma.mostrarEstudianteProyectos(nombreE);
+        }
     }
+
 }
